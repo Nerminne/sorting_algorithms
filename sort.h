241c0f7543ded959cfee8826b7acdb4e10e77e04
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-02-16 23:14:51 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -48,5 +48,6 @@ void PrintArray(int *array, int iBeg, int iEnd);
 void CopyArray(int *array, int iBeg, int iEnd, int *work_copy);
 void TopDownMerge(int *array, int iBeg, int iMid, int iEnd, int *work_copy);
 void TopDownSplitMerge(int *work_copy, int iBeg, int iEnd, int *array);
+void merge_sort(int *array, size_t size);
 
 #endif
