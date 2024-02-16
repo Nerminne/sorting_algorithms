dc4a14e9bbddd731206534f5059b3c1ba76521df
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-02-16 23:25:00 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -63,5 +63,8 @@ void bitonic_compare(bool up, int *x, size_t size);
 void bitonic_merge(bool up, int *x, size_t size, size_t orig_size);
 void bitonic_sort_r(bool up, int *x, size_t size, size_t orig_size);
 void bitonic_sort(int *array, size_t size);
+int hoare_partition(int *array, size_t low, size_t high, size_t size);
+void hoare_quicksort(int *array, size_t low, size_t high, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
