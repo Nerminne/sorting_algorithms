9fa89e0accc307471968dc3adf6df78501ca826b
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-02-16 22:46:57 +0200

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,41 +1,43 @@
 #include "sort.h"
 
 /**
- * shell_sort - sort array with shell method
- * @array: array to sort
- * @size: size of the array
- *
- * Return: nothing
-*/
-
+ * shell_sort - Shell sort algorithm
+ * @array: unsorted data
+ * @size: large of array
+ * Return: Nothing
+ */
 void shell_sort(int *array, size_t size)
 {
-	int in = 0, out = 0, range = 1, aux = 0;
+	int tmp;
+	size_t j, gap, n;
 
+	gap = 1;
 	if (!array || size < 2)
 		return;
-
-	while (range < (int) size / 3)
-		range = range * 3 + 1;
-
-	while (range > 0)
+	/* Create Knuth sequence */
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	/* Start with the largest gap and work down to a gap of 1 */
+	while (gap > 0)
 	{
-		out = range;
-		while (out < (int) size)
+		/* Do a gapped insertion sort for this gap size. */
+		/* The first gap elements array[0..gap-1] are already in gapped order */
+		/* keep adding one more element until the entire array is gap sorted */
+		for (j = gap; j < size; j += 1)
 		{
-			aux = array[out];
-			in = out;
-
-			while (in > range - 1 && array[in - range] >= aux)
-			{
-				array[in] = array[in - range];
-				in = in - range;
-			}
-
-			array[in] = aux;
-			out++;
+			/* add array[j] to the elements that have been gap sorted */
+			/* save array[j] in temp and make a hole at position j */
+			tmp = array[j];
+			/* shift earlier gap-sorted elements up until the correct */
+			/* location for array[j] is found */
+			for (n = j; n >= gap && tmp < array[n - gap]; n -= gap)
+				array[n] = array[n - gap];
+			/* put tmp (the original array[j]) in its correct location */
+			array[n] = tmp;
 		}
+		/* decreasing the interval */
+		gap /= 3;
+		/*  print the array each time you decrease the interval */
 		print_array(array, size);
-		range = (range - 1) / 3;
 	}
 }
