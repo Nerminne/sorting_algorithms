a4195af2e9f96a3d51bb9dc779af3916c4f31b2a
Nerminne <138694722+Nerminne@users.noreply.github.com>
2024-02-16 22:51:16 +0200

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,43 +1,42 @@
 #include "sort.h"
 
 /**
- * shell_sort - Shell sort algorithm
- * @array: unsorted data
- * @size: large of array
- * Return: Nothing
+ * shell_sort - sorts an array of integers in ascending order using a shell
+ * sort algorithm, with gap sizes determined by a decreasing Knuth seqeuence
+ * @array: array of integers to be sorted
+ * @size: amount of elements in array
  */
 void shell_sort(int *array, size_t size)
 {
-	int tmp;
-	size_t j, gap, n;
+	int i, j, gap, n, Knuth_max, temp;
 
-	gap = 1;
 	if (!array || size < 2)
 		return;
-	/* Create Knuth sequence */
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-	/* Start with the largest gap and work down to a gap of 1 */
-	while (gap > 0)
+
+	n = (int)size;
+	for (gap = 1; gap < n; gap = (gap * 3) + 1)
+	{
+		Knuth_max = gap;
+	}
+/* Start with the largest Knuth seq value less than n as gap, */
+/* and work down sequence to a gap of 1 */
+	for (gap = Knuth_max; gap > 0; gap = (gap - 1) / 3)
 	{
 		/* Do a gapped insertion sort for this gap size. */
-		/* The first gap elements array[0..gap-1] are already in gapped order */
-		/* keep adding one more element until the entire array is gap sorted */
-		for (j = gap; j < size; j += 1)
+		for (i = gap; i < n; i++)
 		{
-			/* add array[j] to the elements that have been gap sorted */
-			/* save array[j] in temp and make a hole at position j */
-			tmp = array[j];
-			/* shift earlier gap-sorted elements up until the correct */
-			/* location for array[j] is found */
-			for (n = j; n >= gap && tmp < array[n - gap]; n -= gap)
-				array[n] = array[n - gap];
-			/* put tmp (the original array[j]) in its correct location */
-			array[n] = tmp;
+			/* add array[i] to gap sorted elements; */
+			/* save array[i] in temp in preparation to overwrite */
+			temp = array[i];
+			/* shift earlier gap-sorted elements up until the */
+			/* correct location for array[i] is found */
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+/* temp original array[i]) to its correct location */
+			array[j] = temp;
 		}
-		/* decreasing the interval */
-		gap /= 3;
-		/*  print the array each time you decrease the interval */
 		print_array(array, size);
 	}
 }
